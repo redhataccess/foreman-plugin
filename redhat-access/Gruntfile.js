207fb761c455a55fcea704ef1a3082239639b001
lphiri <lphiri@redhat.com>
2015-04-25 21:05:16 -0400

    add grunt and bower dependency management for insights

---
+++ redhat-access/Gruntfile.js
@@ -0,0 +1,145 @@
+/*global module:false*/
+module.exports = function(grunt) {
+    var path = require('path');
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        // Task configuration.
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            dist: {
+                src: ['lib/<%= pkg.name %>.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true,
+                globals: {}
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib_test: {
+                src: ['lib/**/*.js', 'test/**/*.js']
+            }
+        },
+        nodeunit: {
+            files: ['test/**/*_test.js']
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib_test: {
+                files: '<%= jshint.lib_test.src %>',
+                tasks: ['jshint:lib_test', 'nodeunit']
+            }
+        },
+        bower: {
+            install: {
+                options: {
+                    copy: true,
+                    targetDir: "vendor/assets",
+                    layout :'byType',
+                    // layout: function(type, component, source) {
+                    //     console.log("Type : " + type);
+                    //     console.log("Component : " + component);
+                    //     console.log("Source : " + source);
+                    //     var renamedType = type;
+                    //     if (type == 'js') renamedType = 'javascripts/insights';
+                    //     else if (type == 'css') renamedType = 'stylesheets/insights';
+
+                    //     return path.join(renamedType, component);
+                    // },
+                    clearBowerDir: true
+                }
+            }
+        },
+        sync: {
+            dist: {
+                files: [{
+                        expand: true,
+                        dot: true,
+                        cwd: '.tmp/bower_components/telemetry/release',
+                        dest: 'vendor/assets/images',
+                        src: [
+                            'assets/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+                        ]
+                    }, {
+                        expand: true,
+                        dot: true,
+                        cwd: '.tmp/bower_components/telemetry/release',
+                        dest: 'vendor/assets/fonts',
+                        src: [
+                            'assets/fonts/**/*'
+                        ]
+                    }, {
+                        expand: true,
+                        dot: true,
+                        cwd: '.tmp/bower_components/c3',
+                        dest: 'vendor/assets/javascripts/c3',
+                        src: [
+                            'c3.js'
+                        ]
+                    }, {
+                        expand: true,
+                        dot: true,
+                        cwd: '.tmp/bower_components/d3',
+                        dest: 'vendor/assets/javascripts/d3',
+                        src: [
+                            'd3.js'
+                        ]
+                    },
+
+
+                ]
+            }
+        }
+
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bower-installer');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-sync');
+
+    // Default task.
+    grunt.registerTask('default', ['bower:install','sync']);
+
+
+};
