3e6e0cac7ab4aacca6a34c3bc63db99db063a969
lphiri <lphiri@redhat.com>
2015-05-11 22:19:05 -0400

    disable insights in sam block when in disconnected mode. fix version in user agent

--- redhat-access/app/helpers/redhat_access/analytics_dashboard_helper.rb
+++ redhat-access/app/helpers/redhat_access/analytics_dashboard_helper.rb
@@ -1,19 +1,5 @@
 module RedhatAccess
   module AnalyticsDashboardHelper
-    def is_susbcribed_to_redhat?(org)
-      if org
-        upstream = org.owner_details['upstreamConsumer']
-        return upstream && upstream['idCert']
-      end
-      return false
-    end
-
-    def is_org_selected?
-      return Organization.current
-    end
-
-    def is_telemetry_enabled?
-      true
-    end
+    include RedhatAccess::Telemetry::LookUps
   end
 end
