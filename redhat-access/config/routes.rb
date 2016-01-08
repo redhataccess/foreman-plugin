c3619c5dd9945e0731b95eb86aade4f30b68f713
Jeremy Crafts <jcrafts@redhat.com>
2016-01-08 10:36:05 -0500

    merge  test connection patch

--- redhat-access/config/routes.rb
+++ redhat-access/config/routes.rb
@@ -12,6 +12,7 @@ RedhatAccess::Engine.routes.draw do
     end
     get "/insights/templates/:page" => "analytics_dashboard#template"  # hack to get around angular-rails-templates bug
     scope '/r/insights' do
+      get   '/',                    to: 'api/machine_telemetry_api#api_connection_test'
       get   '/v1/branch_info',      to: 'api/machine_telemetry_api#get_branch_info'
       post  '/uploads/:id',         to: 'api/machine_telemetry_api#proxy_upload'
       match '/view/api/me'   ,      to: 'api/telemetry_api#connection_status', via: [:get]
