5ca42cc9ce9bbbf4f039bbfe23a35145d1f2a366
Jeremy Crafts <jcrafts@redhat.com>
2016-01-08 10:36:05 -0500

    tweak API proxy code for insights client connection test

--- redhat-access/config/routes.rb
+++ redhat-access/config/routes.rb
@@ -15,8 +15,9 @@ RedhatAccess::Engine.routes.draw do
       resource :telemetry_configuration, only: [:show, :update]
     end
     scope '/r/insights' do
+      get   '/',                    to: 'api/machine_telemetry_api#api_connection_test'
       get   '/v1/branch_info',      to: 'api/machine_telemetry_api#get_branch_info'
-      post  '/uploads/:id',         to: 'api/machine_telemetry_api#proxy_upload'
+      post  '/uploads/(:id)',       to: 'api/machine_telemetry_api#proxy_upload'
       match '/view/api/me'   ,      to: 'api/telemetry_api#connection_status'
       match '/view/api/:path',      to: 'api/telemetry_api#proxy', :constraints => {:path => /.*/}
       match '/:path',               to: 'api/machine_telemetry_api#proxy', :constraints => {:path => /.*/}
