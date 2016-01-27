26e09187fcd2c0f53e64a402bd265d40f8658d02
lphiri <lphiri@redhat.com>
2016-01-27 10:04:43 -0500

    Update routes to work with rails 4

--- redhat-access/config/routes.rb
+++ redhat-access/config/routes.rb
@@ -15,8 +15,8 @@ RedhatAccess::Engine.routes.draw do
       get   '/v1/branch_info',      to: 'api/machine_telemetry_api#get_branch_info'
       post  '/uploads/:id',         to: 'api/machine_telemetry_api#proxy_upload'
       match '/view/api/me'   ,      to: 'api/telemetry_api#connection_status', via: [:get]
-      match '/view/api/:path',      to: 'api/telemetry_api#proxy', :constraints => {:path => /.*/} , via: [:all]
-      match '/:path',               to: 'api/machine_telemetry_api#proxy', :constraints => {:path => /.*/}, via: [:all]
+      match '/view/api/:path',      to: 'api/telemetry_api#proxy', :constraints => {:path => /.*/} ,via: [:get, :post, :delete,:put]
+      match '/:path',               to: 'api/machine_telemetry_api#proxy', :constraints => {:path => /.*/}, via: [:get, :post, :delete,:put]
     end
     get 'insights', to: 'analytics_dashboard#index'
     get 'analytics_configuration', to: 'telemetry_configuration#index'
