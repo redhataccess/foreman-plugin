01af5e063a36490e41027881c7f5d759b9a94ca4
lphiri <lphiri@redhat.com>
2016-01-27 15:30:10 -0500

    Fix errors due to host model changes in lookups

--- redhat-access/app/services/redhat_access/telemetry/look_ups.rb
+++ redhat-access/app/services/redhat_access/telemetry/look_ups.rb
@@ -1,34 +1,33 @@
 begin
-  #TODO fix dirty hack
+  # TODO: fix dirty hack
   require '/usr/share/foreman/lib/satellite/version.rb'
 rescue LoadError
-  #don't need to do anything
+  # don't need to do anything
   Rails.logger.debug("Unable to load version file.")
 end
 module RedhatAccess
   module Telemetry
     module LookUps
-
       class  RecordNotFound < StandardError
       end
 
       def can_unregister_system(user)
-        #TODO move this to an auth class?
+        # TODO: move this to an auth class?
         return false if user.nil?
         return true if user.admin
-        permissions = user.cached_roles.collect { |role| role.permissions }.flatten.map! { |permission| permission.name }
-        #Rails.logger.debug("User can unregister telemetry hosts : #{ permissions.include?("rh_telemetry_configurations")}")
+        permissions = user.cached_roles.collect(&:permissions).flatten.map!(&:name)
+        # Rails.logger.debug("User can unregister telemetry hosts : #{ permissions.include?("rh_telemetry_configurations")}")
         # for now we allow all.
         true
       end
 
       def can_mask_rules(user)
         # #TODO move this to an auth class?
-        #TODO move this to an auth class?
+        # TODO move this to an auth class?
         return false if user.nil?
         return true if user.admin
-        permissions = user.cached_roles.collect { |role| role.permissions }.flatten.map! { |permission| permission.name }
-        Rails.logger.debug("User can mask telemetry hosts : #{ permissions.include?("rh_telemetry_configurations")}")
+        permissions = user.cached_roles.collect(&:permissions).flatten.map!(&:name)
+        Rails.logger.debug("User can mask telemetry hosts : #{permissions.include?("rh_telemetry_configurations")}")
         permissions.include?("rh_telemetry_configurations")
       end
 
@@ -37,11 +36,11 @@ module RedhatAccess
           upstream = org.owner_details['upstreamConsumer']
           return upstream && upstream['idCert'] ? true : false
         end
-        return false
+        false
       end
 
       def is_org_selected?
-        Organization.current.nil? ? false: true
+        Organization.current.nil? ? false : true
       end
 
       def get_telemetry_config(org)
@@ -55,7 +54,7 @@ module RedhatAccess
           conf = get_telemetry_config(org)
           return conf.nil? ? false : conf.enable_telemetry
         else
-          raise(RecordNotFound,'Host not found or invalid')
+          raise(RecordNotFound, 'Host not found or invalid')
         end
       end
 
@@ -64,15 +63,15 @@ module RedhatAccess
       end
 
       def get_content_host_by_fqdn(name)
-        Katello::System.first(:conditions => { :name => name})
+        Katello::System.first(:conditions => {:name => name})
       end
 
       def disconnected_org?(org)
         if org
-          #TODO fix hard coding
+          # TODO: fix hard coding
           org.redhat_repository_url != 'https://cdn.redhat.com'
         else
-          raise(RecordNotFound,'Organization not found or invalid')
+          raise(RecordNotFound, 'Organization not found or invalid')
         end
       end
 
@@ -80,7 +79,7 @@ module RedhatAccess
         system = get_content_host(uuid)
         if system.nil?
           ldebug('Host not found or invalid')
-          raise(RecordNotFound,'Host not found or invalid')
+          raise(RecordNotFound, 'Host not found or invalid')
         end
         uuid
       end
@@ -88,40 +87,40 @@ module RedhatAccess
       def get_branch_id_for_org(org)
         if org
           if !org.owner_details['upstreamConsumer'] || !org.owner_details['upstreamConsumer']['uuid']
-            #ldebug('Org manifest not found or invalid in get_branch_id')
-            raise(RecordNotFound,'Branch ID not found for organization')
+            # ldebug('Org manifest not found or invalid in get_branch_id')
+            raise(RecordNotFound, 'Branch ID not found for organization')
           else
             branch_id =  org.owner_details['upstreamConsumer']['uuid']
           end
         else
-          raise(RecordNotFound,'Organization not found or invalid')
+          raise(RecordNotFound, 'Organization not found or invalid')
         end
       end
 
       def get_ssl_options_for_uuid(uuid, ca_file)
         org = get_organization(uuid)
-        get_ssl_options_for_org(org,ca_file)
+        get_ssl_options_for_org(org, ca_file)
       end
 
       def use_basic_auth?
         REDHAT_ACCESS_CONFIG[:enable_telemetry_basic_auth]
       end
 
-      def get_ssl_options_for_org(org ,ca_file)
+      def get_ssl_options_for_org(org, ca_file)
         if org
           verify_peer = REDHAT_ACCESS_CONFIG[:telemetry_ssl_verify_peer] ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE
-          ssl_version = REDHAT_ACCESS_CONFIG[:telemetry_ssl_verify_peer] ?  REDHAT_ACCESS_CONFIG[:telemetry_ssl_verify_peer] : nil
+          ssl_version = REDHAT_ACCESS_CONFIG[:telemetry_ssl_verify_peer] ? REDHAT_ACCESS_CONFIG[:telemetry_ssl_verify_peer] : nil
           ca_file = ca_file ? ca_file : get_default_ssl_ca_file
           Rails.logger.debug("Verify peer #{verify_peer}")
           if use_basic_auth?
             Rails.logger.debug("Using basic auth for portal communication")
-            get_basic_auth_options(org, ca_file,verify_peer, ssl_version)
+            get_basic_auth_options(org, ca_file, verify_peer, ssl_version)
           else
             Rails.logger.debug("Using SSL auth for portal communication")
-            get_mutual_tls_auth_options(org, ca_file,verify_peer, ssl_version)
+            get_mutual_tls_auth_options(org, ca_file, verify_peer, ssl_version)
           end
         else
-          raise(RecordNotFound,'Organization not found or invalid')
+          raise(RecordNotFound, 'Organization not found or invalid')
         end
       end
 
@@ -132,13 +131,13 @@ module RedhatAccess
       def get_mutual_tls_auth_options(org, ca_file, verify_peer, ssl_version)
         upstream = org.owner_details['upstreamConsumer']
         if !upstream || !upstream['idCert'] || !upstream['idCert']['cert'] || !upstream['idCert']['key']
-          raise(RecordNotFound,'Unable to get portal SSL credentials. Missing org manifest?')
+          raise(RecordNotFound, 'Unable to get portal SSL credentials. Missing org manifest?')
         else
           opts = {
             :ssl_client_cert => OpenSSL::X509::Certificate.new(upstream['idCert']['cert']),
-            :ssl_client_key => OpenSSL::PKey::RSA.new(upstream['idCert']['key']),
-            :ssl_ca_file => ca_file,
-            :verify_ssl => verify_peer
+            :ssl_client_key  => OpenSSL::PKey::RSA.new(upstream['idCert']['key']),
+            :ssl_ca_file     => ca_file,
+            :verify_ssl      => verify_peer
           }
           opts[:ssl_version] = ssl_version if ssl_version
           Rails.logger.debug("Telemetry ssl options => ca_file:#{opts[:ssl_ca_file]} , peer verify #{opts[:verify_ssl]}")
@@ -146,12 +145,12 @@ module RedhatAccess
         end
       end
 
-      def get_basic_auth_options(org, ca_file, verify_peer, ssl_version )
+      def get_basic_auth_options(org, ca_file, verify_peer, ssl_version)
         opts = {
-          :user => org.telemetry_configuration.portal_user,
-          :password => org.telemetry_configuration.portal_password,
+          :user        => org.telemetry_configuration.portal_user,
+          :password    => org.telemetry_configuration.portal_password,
           :ssl_ca_file => ca_file,
-          :verify_ssl => verify_peer
+          :verify_ssl  => verify_peer
         }
         opts[:ssl_version] = ssl_version if ssl_version
         opts
@@ -164,21 +163,26 @@ module RedhatAccess
 
       def get_organization(uuid)
         system = get_content_host(uuid)
-        system.nil? ? nil : Organization.find(system.environment.organization_id)
+        system.nil? ? nil : system.organization
       end
 
-      def get_content_host(uuid)
-        system = Katello::System.first(:conditions => { :uuid => uuid })
+      def get_content_host(uuid = nil)
+        uuid ||= params[:id]
+        facet = Katello::Host::SubscriptionFacet.where(:uuid => uuid).first
+        if facet.nil?
+          User.as_anonymous_admin { Resources::Candlepin::Consumer.get(uuid) }
+          raise HttpErrors::NotFound, _("Couldn't find consumer '%s'") % uuid
+        end
+        @host = facet.host
       end
 
       def get_content_hosts(org)
-
         if org
           org_id = org.id
           environment_ids = Organization.find(org_id).kt_environments.pluck(:id)
-          hosts =  Katello::System.readable.where({:environment_id => environment_ids}).pluck(:uuid).compact.sort
+          hosts =  Katello::System.readable.where(:environment_id => environment_ids).pluck(:uuid).compact.sort
         else
-          raise(RecordNotFound,'Organization not found or invalid')
+          raise(RecordNotFound, 'Organization not found or invalid')
         end
       end
 
@@ -195,10 +199,10 @@ module RedhatAccess
           uri.password = proxy_config.password
           proxy = uri.to_s
         end
-        return proxy
+        proxy
       end
 
-      #TODO move version and name methods to generic utility
+      # TODO: move version and name methods to generic utility
       def get_rha_plugin_name
         'redhat_access'
       end
@@ -224,8 +228,6 @@ module RedhatAccess
         end
         Foreman::Version.new.to_s
       end
-
-
     end
   end
 end
