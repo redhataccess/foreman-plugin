66471bf1221ee271c783722bc3a9ec117e6c7443
lphiri <lphiri@redhat.com>
2016-09-01 14:31:21 -0400

    Fixes #4

--- redhat-access/app/controllers/redhat_access/telemetry_configurations_controller.rb
+++ redhat-access/app/controllers/redhat_access/telemetry_configurations_controller.rb
@@ -14,17 +14,24 @@ module RedhatAccess
       conf = get_telemetry_config(Organization.current)
       if conf
         begin
-          if conf.update_attributes(JSON.parse(request.body.read))
+          if conf.update_attributes(telemetry_configuration_params)
           render json: {:message => "config updated"}
         else
           render json: {:error=>"Invalid parameters"}.to_json, status: 400
         end
-        rescue=>e
+          rescue=>e
+          Rails.logger.info(e)
           render json: {:error=>"Error processing update"}.to_json, status: 500
         end
       else
         render json: {:error=>"Configurationnotfound"}.to_json, status: 404
       end
+    end
+
+    private
+
+    def telemetry_configuration_params
+      params.require(:telemetry_configuration).permit(:enable_telemetry, :portal_password, :portal_user, :organization_id, :email)
     end
 
   end
