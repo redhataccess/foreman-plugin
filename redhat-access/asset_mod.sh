6028082910730b181976f78b2639a0066d187e21
Lindani Phiri <lphiri@redhat.com>
2017-11-01 16:06:34 -0400

    Missing asset

--- redhat-access/asset_mod.sh
+++ redhat-access/asset_mod.sh
@@ -4,3 +4,4 @@ sed -i -r 's/assets\/images/insights/g' vendor/assets/stylesheets/telemetry/sat6
 sed -i -r 's/static\/images\/i_risk-of-change.svg/\/assets\/insights\/i_risk-of-change.svg/g' vendor/assets/javascripts/telemetry/insights.js
 sed -i -r 's/static\/images\/l_ansible-unsupported.svg/\/assets\/insights\/l_ansible-unsupported.svg/g' vendor/assets/javascripts/telemetry/insights.js
 sed -i -r 's/static\/images\/l_ansible-blue.svg/\/assets\/insights\/l_ansible-blue.svg/g' vendor/assets/javascripts/telemetry/insights.js
+sed -i -r 's/static\/images\/l_ansible-white.svg/\/assets\/insights\/l_ansible-white.svg/g' vendor/assets/javascripts/telemetry/insights.js
