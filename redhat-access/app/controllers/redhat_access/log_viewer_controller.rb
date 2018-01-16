1bec07786dd95116f19ef9ad6512f3e8f057caf2
Lindani Phiri <lphiri@redhat.com>
2018-01-16 12:49:49 -0500

    BZ1495483

--- redhat-access/app/controllers/redhat_access/log_viewer_controller.rb
+++ redhat-access/app/controllers/redhat_access/log_viewer_controller.rb
@@ -3,6 +3,7 @@ require_dependency "redhat_access/application_controller"
 module RedhatAccess
   class LogViewerController < ApplicationController
     def index
+      require_admin
       redirect_to '/redhat_access/#/logviewer'
     end
   end
